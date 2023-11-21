557683152d118c88dbc8df76de88fb5dd9e1be65
Carlos Alberto Pastor Marfurt <carlospm19@ttc.cat>
2023-11-21 18:39:12 +0100

    123

---
+++ Practica 2 - Lab.R
@@ -0,0 +1,4 @@
+library(XML)
+#1. Descargar la página web de la URL indicada, y almacenarlo en un formato de R apto para ser tratado.
+#El primer paso para realizar tareas de crawling y scraping es poder descargar los datos de la web. Para esto usaremos la capacidad de R y 
+#de sus librerías (httr y XML) para descargar webs y almacenarlas en variables que podamos convertir en un formato fácil de analizar (p.e. de HTML a XML).
